5465f8e624592f26d77ddc5141c7692e431675aa
Jesse Newland <jnewland@gmail.com>
2009-01-13 12:37:42 -0500

    better template

--- templates/moonshine.rb
+++ templates/moonshine.rb
@@ -2,4 +2,15 @@ class <%= server_name.capitalize %>MoonshineServer < Moonshine::Manifest::Rails
   #the user to run non-privedged tasks as.
   #if this user doesn't exist, one will be created for you.
   user "rails"
+
+  #install packages needed by your rails app
+  #packages %w(foo bar baz)
+
+  #services needed by your rails app, and an array of packages they depend on
+  #service('memcached', %w(memcached libmemcache0 libmemcache-dev))
+
+  #rubified puppet manifests
+  #role :something_else do
+  #  exec "foo", :command => "echo 'normal puppet stuff here' > /tmp/test"
+  #end
 end
