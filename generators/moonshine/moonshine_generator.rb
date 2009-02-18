c3ee92de335cfeecaade349b10e03e107b4e9f51
Jesse Newland <jnewland@gmail.com>
2009-02-18 09:32:38 -0500

    application is the default name, manifests go in app/manifests

--- generators/moonshine/moonshine_generator.rb
+++ generators/moonshine/moonshine_generator.rb
@@ -4,8 +4,7 @@ class MoonshineGenerator < Rails::Generator::Base
   attr_reader :file_name, :klass_name
 
   def initialize(runtime_args, runtime_options = {})
-    name = runtime_args.shift
-    puts banner and exit(1) unless name
+    name = runtime_args.shift || 'application'
     @file_name = name.downcase.underscore + "_manifest"
     @klass_name = @file_name.classify + "Manifest"
     super
@@ -13,20 +12,9 @@ class MoonshineGenerator < Rails::Generator::Base
 
   def manifest
     record do |m|
-      m.directory 'config/moonshine'
-      m.template  'moonshine.rb',        "config/moonshine/#{file_name}.rb"
+      m.directory 'app/manifests'
+      m.template  'moonshine.rb',        "app/manifests/#{file_name}.rb"
     end
-  end
-
-protected
-
-  def banner
-    """Usage: #{$0} server SERVERNAME
-
-Example:
-
-
-  #{$0} server MyAppMain"""
   end
 
 end
