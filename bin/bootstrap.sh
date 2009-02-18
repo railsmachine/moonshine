099d06783975a8abc1dcba6e06ae2c7e929ff306
Jesse Newland <jnewland@gmail.com>
2009-02-18 11:36:42 -0500

    psuedo idempotency for the bootstrap

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,59 +1,61 @@
 #!/bin/bash
 
-# check for ruby and bail out.
-if [ `which ruby` ]; then
- echo "Ruby already installed."
- exit
-fi
-
 echo "***Bootstrappin' the system!***"
 apt-get update
 
 echo "Installing build packages"
 apt-get install -q -y build-essential zlib1g-dev libssl-dev libreadline5-dev wget
 
-PREFIX="/usr"
-REE="ruby-enterprise-1.8.6-20090201"
+if [ `which ruby` ]; then
+ echo "Ruby already installed."
+else
+  PREFIX="/usr"
+  REE="ruby-enterprise-1.8.6-20090201"
 
-pushd /tmp
-echo "Downloading REE"
-wget -q http://rubyforge.org/frs/download.php/51100/$REE.tar.gz
-echo "Untar REE"
-tar xvzf $REE.tar.gz
-pushd $REE/
+  pushd /tmp
+  echo "Downloading REE"
+  wget -q http://rubyforge.org/frs/download.php/51100/$REE.tar.gz
+  echo "Untar REE"
+  tar xvzf $REE.tar.gz
+  pushd $REE/
 
-echo "Installing fancy google memory thingie"
-pushd ./source/vendor/google-perftools-0.99.2
-./configure --prefix=$PREFIX --disable-dependency-tracking
-make libtcmalloc_minimal.la
-rm -f $PREFIX/lib/libtcmalloc_minimal*.so*
-cp -pf .libs/libtcmalloc_minimal*.so* $PREFIX/lib/
-popd
+  echo "Installing fancy google memory thingie"
+  pushd ./source/vendor/google-perftools-0.99.2
+  ./configure --prefix=$PREFIX --disable-dependency-tracking
+  make libtcmalloc_minimal.la
+  rm -f $PREFIX/lib/libtcmalloc_minimal*.so*
+  cp -pf .libs/libtcmalloc_minimal*.so* $PREFIX/lib/
+  popd
 
-echo "Makeing and installing REE"
-pushd ./source
-./configure --prefix=$PREFIX
-echo 'Avert your eyes'
-# YIKES!
-sed -i 's?LIBS = -ldl?LIBS = $(PRELIBS) -ldl?' ./Makefile
-make PRELIBS="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -ltcmalloc_minimal"
-make install
-popd
+  echo "Makeing and installing REE"
+  pushd ./source
+  ./configure --prefix=$PREFIX
+  echo 'Avert your eyes'
+  # YIKES!
+  sed -i 's?LIBS = -ldl?LIBS = $(PRELIBS) -ldl?' ./Makefile
+  make PRELIBS="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -ltcmalloc_minimal"
+  make install
+  popd
 
-echo "Installing RubyGems"
-pushd ./rubygems
-ruby setup.rb --no-rdoc --no-ri
-popd
+  echo "Installing RubyGems"
+  pushd ./rubygems
+  ruby setup.rb --no-rdoc --no-ri
+  popd
 
-echo "Cleaning up REE download"
-rm -rf $REE*
-popd
+  echo "Cleaning up REE download"
+  rm -rf $REE*
+  popd
+fi
 
-echo "Installing Moonshine"
-gem install puppet --no-rdoc --no-ri
-gem install facter --version 1.5.2 --no-rdoc --no-ri
-# do this until facter 1.5.4 is released and fixes ubuntu bug
-gem uninstall facter --version 1.5.3
-gem install shadow_puppet shadow_facter --no-rdoc --no-ri
-#gem install moonshine --no-rdoc --no-ri
-echo "Install Moonshine Manually!"
+if [ `which moonshine` ]; then
+ echo "Moonshine already installed."
+else
+  echo "Installing Moonshine"
+  gem install puppet --no-rdoc --no-ri
+  gem install facter --version 1.5.2 --no-rdoc --no-ri
+  # do this until facter 1.5.4 is released and fixes ubuntu bug
+  gem uninstall facter --version 1.5.3
+  gem install shadow_puppet shadow_facter --no-rdoc --no-ri
+  #gem install moonshine --no-rdoc --no-ri
+  echo "Install Moonshine Manually!"
+fi
