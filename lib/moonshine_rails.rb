54714c9142207d1c7041b28e563d5f12dce6479d
Jesse Newland <jnewland@gmail.com>
2009-02-19 15:31:44 -0500

    typo

--- lib/moonshine_rails.rb
+++ lib/moonshine_rails.rb
@@ -2,4 +2,5 @@ module Moonshine
   module Manifest
   end
 end
-require "#{File.dirname(__FILE__)}/moonshine/mainfest/rails.rb"
+require 'shadow_puppet'
+require "#{File.dirname(__FILE__)}/moonshine/manifest/rails.rb"
