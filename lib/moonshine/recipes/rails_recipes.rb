4fa526d3f233e890dc7bdf159d7523a44d3509d9
Jesse Newland <jnewland@gmail.com>
2009-02-20 16:21:48 -0500

    fix bootstrap

--- lib/moonshine/recipes/rails_recipes.rb
+++ lib/moonshine/recipes/rails_recipes.rb
@@ -1,14 +1,14 @@
 module RailsRecipes
   def bootstrap_database
     exec "boostrap_database", { :command => 'rake db:schema:load',
-                             :cwd => working_directory,
+                             :cwd => self.class.working_directory,
                              :environment => "RAILS_ENV=#{fetch(:rails_env, 'production')}",
                              :refreshonly => true}
   end
 
   def migrations
     exec "migrations", { :command => 'rake db:migrate',
-                             :cwd => working_directory,
+                             :cwd => self.class.working_directory,
                              :environment => "RAILS_ENV=#{fetch(:rails_env, 'production')}"}
   end
 end
