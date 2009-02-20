b4ffbb3bd079ca3aa8455626bad714c38070a59a
Jesse Newland <jnewland@gmail.com>
2009-02-20 16:25:09 -0500

    env

--- lib/moonshine/recipes/rails_recipes.rb
+++ lib/moonshine/recipes/rails_recipes.rb
@@ -2,13 +2,13 @@ module RailsRecipes
   def bootstrap_database
     exec "boostrap_database", { :command => 'rake db:schema:load',
                              :cwd => self.class.working_directory,
-                             :environment => "RAILS_ENV=#{fetch(:rails_env, 'production')}",
+                             :environment => "RAILS_ENV=#{ENV['RAILS_ENV']}",
                              :refreshonly => true}
   end
 
   def migrations
     exec "migrations", { :command => 'rake db:migrate',
                              :cwd => self.class.working_directory,
-                             :environment => "RAILS_ENV=#{fetch(:rails_env, 'production')}"}
+                             :environment => "RAILS_ENV=#{ENV['RAILS_ENV']}"}
   end
 end
