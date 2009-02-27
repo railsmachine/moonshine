aca943b92e0de6025c998d13d3f7bf14cda26ccd
Jesse Newland <jnewland@gmail.com>
2009-02-27 10:50:56 -0500

    add environment back in

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 ENV['RAILS_ENV'] = 'test'
 ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '/../../../..'
 require 'test/unit'
+require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config/environment.rb'))
 require 'moonshine'
 require 'mocha'
