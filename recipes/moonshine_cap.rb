4930738494a0185f2a0402d775b9b53418cc13f0
Jesse Newland <jnewland@gmail.com>
2009-03-09 10:10:26 -0400

    fix some cap recipes

--- recipes/moonshine_cap.rb
+++ recipes/moonshine_cap.rb
@@ -64,37 +64,44 @@ namespace :moonshine do
     desc "remotely console"
     task :console, :roles => :app, :except => {:no_symlink => true} do
       input = ''
-      run "cd #{current_release} && ./script/console #{fetch(:rails_env, 'production')}" do |channel, stream, data|
+      run "cd #{current_path} && ./script/console #{fetch(:rails_env, "production")}" do |channel, stream, data|
         next if data.chomp == input.chomp || data.chomp == ''
         print data
         channel.send_data(input = $stdin.gets) if data =~ /^(>|\?)>/
       end
     end
 
-    desc "tail production log files"
+    desc "Show requests per second"
+    task :rps, :roles => :app, :except => {:no_symlink => true} do
+      count = 0
+      last = Time.now
+      run "tail -f #{shared_path}/log/#{fetch(:rails_env, "production")}.log" do |ch, stream, out|
+        break if stream == :err
+        count += 1 if out =~ /^Completed in/
+        if Time.now - last >= 1
+          puts "#{ch[:host]}: %2d Requests / Second" % count
+          count = 0
+          last = Time.now
+        end
+      end
+    end
+
+    desc "tail application log file"
     task :log, :roles => :app, :except => {:no_symlink => true} do
-      run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
+      run "tail -f #{shared_path}/log/#{fetch(:rails_env, "production")}.log" do |channel, stream, data|
         puts "#{data}"
         break if stream == :err
       end
     end
 
-    desc "tail apache error log"
-    task :error_log, :roles => :app, :except => {:no_symlink => true} do
-      run "/var/log/apache2/error.log" do |channel, stream, data|
-        puts "#{data}"
+    desc "tail vmstat"
+    task :vmstat, :roles => [:web, :db] do
+      run "vmstat 5" do |channel, stream, data|
+        puts "[#{channel[:host]}]"
+        puts data.gsub(/\s+/, "\t")
         break if stream == :err
       end
     end
-
-    desc "tail apache access log"
-    task :access_log, :roles => :app, :except => {:no_symlink => true} do
-      run "/var/log/apache2/access.log" do |channel, stream, data|
-        puts "#{data}"
-        break if stream == :err
-      end
-    end
-  end
 
   task :update_and_console do
     set :moonshine_apply, false
