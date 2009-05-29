bd0b0c63eae49d5a94c141694dff0152a7f6aa1b
jnewland <jnewland@gmail.com>
2009-05-28 18:56:53 -0700

    support Ubuntu 8.04 or greater

--- lib/moonshine/manifest/rails.rb
+++ lib/moonshine/manifest/rails.rb
@@ -2,11 +2,11 @@
 #running on Ubuntu 8.10 or greater.
 class Moonshine::Manifest::Rails < Moonshine::Manifest
   def validate_platform
-    unless Facter.lsbdistid == 'Ubuntu' && Facter.lsbdistrelease.to_f >= 8.1
+    unless Facter.lsbdistid == 'Ubuntu' && Facter.lsbdistrelease.to_f >= 8.04
       error = <<-ERROR
 
 
-      Moonshine::Manifest::Rails is currently only supported on Ubuntu 8.10
+      Moonshine::Manifest::Rails is currently only supported on Ubuntu 8.04
       or greater. If you'd like to see your favorite distro supported, fork
       Moonshine on GitHub!
       ERROR
