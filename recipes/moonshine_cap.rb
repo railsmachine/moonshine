41eb65a37c3781b2e28d8434f95f1a1165c358b4
Jesse Newland <jnewland@gmail.com>
2009-09-11 09:18:48 -0400

    re-work ruby install process and add support for ruby 1.8.7

--- recipes/moonshine_cap.rb
+++ recipes/moonshine_cap.rb
@@ -30,6 +30,8 @@ namespace :moonshine do
   dependencies. Called by deploy:setup.
   DESC
   task :bootstrap do
+    ruby.install
+    vcs.install
     begin
       config = YAML.load_file(File.join(Dir.pwd, 'config', 'moonshine.yml'))
       put(YAML.dump(config),"/tmp/moonshine.yml")
@@ -39,10 +41,6 @@ namespace :moonshine do
       exit(0)
     end
     put(File.read(File.join(File.dirname(__FILE__), '..', 'lib', 'moonshine_setup_manifest.rb')),"/tmp/moonshine_setup_manifest.rb")
-    put(File.read(File.join(File.dirname(__FILE__), "bootstrap.#{fetch(:ruby, 'ree')}.sh")),"/tmp/bootstrap.sh")
-    sudo 'chmod a+x /tmp/bootstrap.sh'
-    sudo "FORCE_RUBY=#{fetch(:force_ruby,'false')} /tmp/bootstrap.sh"
-    sudo 'rm /tmp/bootstrap.sh'
     sudo "shadow_puppet /tmp/moonshine_setup_manifest.rb"
     sudo 'rm /tmp/moonshine_setup_manifest.rb'
     sudo 'rm /tmp/moonshine.yml'
@@ -192,16 +190,96 @@ namespace :deploy do
   DESC
   task :setup, :except => { :no_release => true } do
     moonshine.bootstrap
-    vcs.install
   end
 end
 
 namespace :ruby do
+
   desc "Forces a reinstall of Ruby and restarts Apache/Passenger"
   task :upgrade do
-    set :force_ruby, 'true'
-    moonshine.bootstrap
+    install
     apache.restart
+  end
+
+  desc "Install Ruby + Rubygems"
+  task :install do
+    install_deps
+    send fetch(:ruby, 'ree').intern
+    install_rubygems
+    install_moonshine_deps
+  end
+
+  task :mri do
+    apt
+  end
+
+  task :apt do
+    sudo "apt-get install -q -y ruby-full"
+  end
+
+  task :remove_ruby_from_apt do
+    sudo "apt-get remove -q -y ^.*ruby.* || true"
+    #TODO apt-pinning to ensure ruby is never installed via apt
+  end
+
+  task :ree do
+    remove_ruby_from_apt
+    run [
+      'cd /tmp',
+      'rm -rf ruby-enterprise-1.8.6-20090610* || true',
+      'wget -q http://assets.railsmachine.com/other/ruby-enterprise-1.8.6-20090610.tar.gz',
+      'tar xzf ruby-enterprise-1.8.6-20090610.tar.gz',
+      'sudo /tmp/ruby-enterprise-1.8.6-20090610/installer --dont-install-useful-gems -a /usr'
+    ].join(" && ")
+  end
+
+  task :ree187 do
+    remove_ruby_from_apt
+    run [
+      'cd /tmp',
+      'rm -rf ruby-enterprise-1.8.7-2009.10.tar.gz* || true',
+      'wget -q http://rubyforge.org/frs/download.php/66162/ruby-enterprise-1.8.7-2009.10.tar.gz',
+      'tar xzf ruby-enterprise-1.8.7-2009.10.tar.gz',
+      'sudo /tmp/ruby-enterprise-1.8.7-2009.10/installer --dont-install-useful-gems -a /usr'
+    ].join(" && ")
+  end
+
+  task :src187 do
+    remove_ruby_from_apt
+    run [
+      'cd /tmp',
+      'rm -rf ruby-1.8.7-p174* || true',
+      'wget -q ftp://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.7-p174.tar.bz2',
+      'tar xjf ruby-1.8.7-p174.tar.bz2',
+      'cd /tmp/ruby-1.8.7-p174',
+      './configure --prefix=/usr',
+      'make',
+      'sudo make install'
+    ].join(" && ")
+  end
+
+  task :install_rubygems do
+    run [
+      'cd /tmp',
+      'rm -rf rubygems-1.3.5* || true',
+      'wget -q http://rubyforge.org/frs/download.php/60718/rubygems-1.3.5.tgz',
+      'tar xfz rubygems-1.3.5.tgz',
+      'cd /tmp/rubygems-1.3.5',
+      'sudo ruby setup.rb',
+      'sudo ln -s /usr/bin/gem1.8 /usr/bin/gem || true',
+      'gem update --system'
+    ].join(" && ")
+  end
+
+  task :install_deps do
+    sudo "apt-get update"
+    sudo "apt-get install -q -y build-essential zlib1g-dev libssl-dev libreadline5-dev wget"
+  end
+
+  task :install_moonshine_deps do
+    sudo "gem install rake --no-rdoc --no-ri"
+    sudo "gem install puppet -v 0.24.8 --no-rdoc --no-ri"
+    sudo "gem install shadow_puppet --no-rdoc --no-ri"
   end
 end
 
@@ -220,6 +298,6 @@ namespace :vcs do
       when 'git' then 'git-core'
       else scm.to_s
     end
-    sudo "apt-get -qq -y install #{package}"
+    sudo "apt-get -qq -y install #{package}" unless package == 'none'
   end
 end
