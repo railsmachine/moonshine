fcce9f0b90cec8862b39c96d7c27689846abcd9a
Ches Martin <ches@whiskeyandgrits.net>
2009-11-09 20:54:22 +0700

    Strip trailing whitespace from generator templates

--- generators/moonshine/templates/moonshine.rb
+++ generators/moonshine/templates/moonshine.rb
@@ -1,13 +1,13 @@
 require "#{File.dirname(__FILE__)}/../../vendor/plugins/moonshine/lib/moonshine.rb"
 class <%= klass_name %> < Moonshine::Manifest::Rails
   # The majority of your configuration should be in <tt>config/moonshine.yml</tt>
-  # If necessary, you may provide extra configuration directly in this class 
-  # using the configure method. The hash passed to the configure method is deep 
-  # merged with what is in <tt>config/moonshine.yml</tt>. This could be used, 
-  # for example, to store passwords and/or private keys outside of your SCM, or 
+  # If necessary, you may provide extra configuration directly in this class
+  # using the configure method. The hash passed to the configure method is deep
+  # merged with what is in <tt>config/moonshine.yml</tt>. This could be used,
+  # for example, to store passwords and/or private keys outside of your SCM, or
   # to query a web service for configuration data.
   #
-  # In the example below, the value configuration[:custom][:random] can be used in 
+  # In the example below, the value configuration[:custom][:random] can be used in
   # your moonshine settings or templates.
   #
   # require 'net/http'
@@ -17,7 +17,7 @@ class <%= klass_name %> < Moonshine::Manifest::Rails
   #   :custom => { :random => random  }
   # })
 
-  # The default_stack recipe install Rails, Apache, Passenger, the database from 
+  # The default_stack recipe install Rails, Apache, Passenger, the database from
   # database.yml, Postfix, Cron, logrotate and NTP. See lib/moonshine/manifest/rails.rb
   # for details. To customize, remove this recipe and specify the components you want.
   recipe :default_stack
@@ -27,23 +27,23 @@ class <%= klass_name %> < Moonshine::Manifest::Rails
     # If you've already told Moonshine about a package required by a gem with
     # :apt_gems in <tt>moonshine.yml</tt> you do not need to include it here.
     # package 'some_native_package', :ensure => :installed
-    
+
     # some_rake_task = "/usr/bin/rake -f #{configuration[:deploy_to]}/current/Rakefile custom:task RAILS_ENV=#{ENV['RAILS_ENV']}"
     # cron 'custom:task', :command => some_rake_task, :user => configuration[:user], :minute => 0, :hour => 0
-    
+
     # %w( root rails ).each do |user|
     #   mailalias user, :recipient => 'you@domain.com'
     # end
-    
+
     # farm_config = <<-CONFIG
     #   MOOCOWS = 3
     #   HORSIES = 10
     # CONFIG
     # file '/etc/farm.conf', :ensure => :present, :content => farm_config
-    
+
     # Logs for Rails, MySQL, and Apache are rotated by default
     # logrotate '/var/log/some_service.log', :options => %w(weekly missingok compress), :postrotate => '/etc/init.d/some_service restart'
-    
+
     # Only run the following on the 'testing' stage using capistrano-ext's multistage functionality.
     # on_stage 'testing' do
     #   file '/etc/motd', :ensure => :file, :content => "Welcome to the TEST server!"
