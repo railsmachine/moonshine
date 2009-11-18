5a16b398d98e2d14e95e0507461977c8b1fc4d27
Jesse Newland <jnewland@gmail.com>
2009-11-18 13:45:48 -0500

    introduce shared_config as the default instead of local_config

--- recipes/moonshine_cap.rb
+++ recipes/moonshine_cap.rb
@@ -33,6 +33,7 @@ namespace :moonshine do
     ruby.install
     vcs.install
     moonshine.setup_directories
+    shared_config.upload
   end
 
   desc <<-DESC
@@ -76,6 +77,7 @@ namespace :moonshine do
   after 'deploy:finalize_update' do
     local_config.upload
     local_config.symlink
+    shared_config.symlink
     app.symlinks.update
   end
 
@@ -167,6 +169,44 @@ namespace :local_config do
     end
   end
   
+end
+
+namespace :shared_config do
+  desc <<-DESC
+  Uploads local configuration files to the application's shared directory for
+  later symlinking (if necessary). Called if shared_config is set.
+  DESC
+  task :upload do
+    fetch(:shared_config, []).each do |file|
+      filename = File.split(file).last
+      if File.exist?(file)
+        put File.read(file), "#{ shared_path }/config/#{ filename }"
+      end
+    end
+  end
+
+  desc <<-DESC
+  Downloads remote configuration from the application's shared directory for
+  local use.
+  DESC
+  task :download do
+    fetch(:shared_config, []).each do |file|
+      filename = File.split(file).last
+      if File.exist?(file)
+        get "#{ shared_path }/config/#{ filename }", file
+      end
+    end
+  end
+
+  desc <<-DESC
+  Symlinks uploaded local configurations into the release directory.
+  DESC
+  task :symlink do
+    fetch(:shared_config, []).each do |file|
+      filename = File.split(file).last
+      run "ls #{ latest_release }/#{ file } 2> /dev/null || ln -nfs #{ shared_path }/config/#{ filename } #{ latest_release }/#{ file }"
+    end
+  end
 end
 
 namespace :deploy do
