ecbc2df24109cf47de8d45f6c595932fcad04a0b
Ches Martin <ches@whiskeyandgrits.net>
2010-04-09 07:33:37 +0700

    Don't use deprecated plugin method for generated specs

--- generators/moonshine_plugin/templates/spec.rb
+++ generators/moonshine_plugin/templates/spec.rb
@@ -1,24 +1,20 @@
 require File.join(File.dirname(__FILE__), 'spec_helper.rb')
 
-class <%= module_name %>Manifest < Moonshine::Manifest
-  plugin :<%= name %>
-end
-
 describe "A manifest with the <%= module_name %> plugin" do
-  
+
   before do
     @manifest = <%= module_name %>Manifest.new
     @manifest.<%= name %>
   end
-  
+
   it "should be executable" do
     @manifest.should be_executable
   end
-  
+
   #it "should provide packages/services/files" do
   # @manifest.packages.keys.should include 'foo'
   # @manifest.files['/etc/foo.conf'].content.should match /foo=true/
   # @manifest.execs['newaliases'].refreshonly.should be_true
   #end
-  
+
 end
