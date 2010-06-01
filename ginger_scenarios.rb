15b6bb5b4850256870278ca5a3261043cf1149b1
Joshua Nichols <josh@technicalpickles.com>
2010-06-01 00:53:44 -0400

    Added a few more ginger scenarios for recent rails releases.

--- ginger_scenarios.rb
+++ ginger_scenarios.rb
@@ -13,9 +13,11 @@ Ginger.configure do |config|
   config.aliases["rails"] = "rails"
   
   rails_2_3_5 = create_scenario('2.3.5')
+  rails_2_3_8 = create_scenario('2.3.8')
 
   rails_3_0_0_beta = create_scenario('3.0.0.beta')
+  rails_3_0_0_beta3 = create_scenario('3.0.0.beta3')
   
-  config.scenarios << rails_2_3_5 << rails_3_0_0_beta
+  config.scenarios << rails_2_3_5 << rails_2_3_8 << rails_3_0_0_beta << rails_3_0_0_beta3
 end
 
