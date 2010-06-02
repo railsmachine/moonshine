89a12f16275b64fa2783c22109d63730f13b69c1
Joshua Nichols <josh@technicalpickles.com>
2010-06-02 13:02:19 -0400

    Added more explicit multistage configuration to generated config/deploy.rb. Also use smarter erb tags to avoid extraneous whitespace there.

--- generators/moonshine/templates/deploy.rb
+++ generators/moonshine/templates/deploy.rb
@@ -1,5 +1,7 @@
-<%- if options[:multistage] %>
-require 'capistrano/ext/multistage'
-<%- else %>
+<%- if options[:multistage] -%>
+set :stages, %w(staging production)
+set :default_stage, 'staging'
+require 'capistrano/ext/multistage' rescue 'YOU NEED TO INSTALL THE capistrano-ext GEM'
+<%- else -%>
 server '<%= domain %>', :app, :web, :db, :primary => true
-<%- end %>
+<%- end -%>
