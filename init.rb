b3e28db2a675182532a5ae2c9f3922cfe76df8c4
Joshua Nichols <josh@technicalpickles.com>
2010-06-07 17:54:49 -0400

    Added fix for manifests being eagerly loaded.

--- init.rb
+++ init.rb
@@ -1,1 +1,4 @@
-#we don't need to load anything for rails just yet
+if Rails::VERSION::MAJOR > 2
+  # Make app/manifests NOT be eagerly loaded
+  Rails.configuration.paths.app.manifests 'app/manifests', :eager_load => false
+end
