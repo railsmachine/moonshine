db3933d1635f92692c3e79bb584ed672791384e7
Joshua Nichols <josh@technicalpickles.com>
2010-06-26 03:56:21 -0400

    Removed explicit installation of puppet, as it's a dependency of shadow_puppet.

--- lib/moonshine/capistrano_integration.rb
+++ lib/moonshine/capistrano_integration.rb
@@ -377,7 +377,6 @@ module Moonshine
 
           task :install_moonshine_deps do
             sudo 'gem install rake --no-rdoc --no-ri'
-            sudo 'gem install puppet -v 0.24.8 --no-rdoc --no-ri'
             sudo 'gem install shadow_puppet --no-rdoc --no-ri'
           end
         end
