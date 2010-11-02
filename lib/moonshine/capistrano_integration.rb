c358345dc0ce1465fbb34fbc8dcc244467a5ad25
Joshua Nichols <josh@technicalpickles.com>
2010-11-02 11:32:17 -0400

    Fix 'apt-get update' to not run on EOL'd 8.04

--- lib/moonshine/capistrano_integration.rb
+++ lib/moonshine/capistrano_integration.rb
@@ -434,7 +434,7 @@ module Moonshine
 
         namespace :aptget do
           task :update do
-            sudo 'apt-get update'
+            sudo 'source /etc/lsb-release; if test ${DISTRIB_RELEASE} != 8.10; then apt-get update; fi'
           end
         end
       end
