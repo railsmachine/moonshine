281935161a4d5e8e4d93df35c32469971161f669
Will Farrington <will@railsmachine.com>
2011-01-25 09:50:28 -0500

    Revert "Ensure all rake tasks preserve their exit code."
    This reverts commit 7d612aaa55eacaefb1e15860b66e279d320654c0.

--- lib/moonshine/manifest/rails/rails.rb
+++ lib/moonshine/manifest/rails/rails.rb
@@ -270,7 +270,7 @@ module Moonshine::Manifest::Rails::Rails
   # app, with RAILS_ENV properly set
   def rake(name, options = {})
     exec("rake #{name}", {
-      :command => "rake #{name} 2>&1 | tee -a /var/log/moonshine_rake.log; test ${PIPESTATUS: -1} = 0",
+      :command => "rake #{name} 2>&1 | tee -a /var/log/moonshine_rake.log",
       :user => configuration[:user],
       :cwd => rails_root,
       :environment => "RAILS_ENV=#{ENV['RAILS_ENV']}",
