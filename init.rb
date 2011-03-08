2a5e8844d1623972f78ea12522c67a95e6df5c24
Will Farrington <will@railsmachine.com>
2011-03-08 10:40:27 -0500

    Rails 3 doesn't like our load-order work-around, so let's just have the generator do it.

--- init.rb
+++
@@ -1,4 +0,0 @@
-if Rails::VERSION::MAJOR > 2
-  # Make app/manifests NOT be eagerly loaded
-  Rails.configuration.paths.app.manifests('app/manifests').skip_eager_load!
-end
--- lib/generators/moonshine/moonshine_generator.rb
+++ lib/generators/moonshine/moonshine_generator.rb
@@ -19,6 +19,7 @@ class MoonshineGenerator < Rails::Generators::Base
     template "moonshine.rb", "app/manifests/#{file_name}.rb"
     template "moonshine.yml", "config/moonshine.yml"
     template "deploy.rb", "config/deploy.rb"
+    environment "config.paths.app.manifests 'app/manifests', :eager_load => false", :verbose => true
 
     if options[:multistage]
       template 'staging-deploy.rb', 'config/deploy/staging.rb'
