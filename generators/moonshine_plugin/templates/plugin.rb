74952ab55b0042662bfa7fc9ae84b73a0fe6edae
Josh Nichols <josh@railsmachine.com>
2011-04-12 23:46:10 -0400

    Fix module namespacing in newly generated plugin.

--- generators/moonshine_plugin/templates/plugin.rb
+++ generators/moonshine_plugin/templates/plugin.rb
@@ -1,19 +1,21 @@
-module <%= module_name %>
+module Moonshine
+  module <%= module_name %>
 
-  # Define options for this plugin via the <tt>configure</tt> method
-  # in your application manifest:
-  #
-  #    configure(:<%= name %> => {:foo => true})
-  #
-  # Moonshine will autoload plugins, just call the recipe(s) you need in your
-  # manifests:
-  #
-  #    recipe :<%= name %>
-  def <%= name %>(options = {})
-    # define the recipe
-    # options specified with the configure method will be 
-    # automatically available here in the options hash.
-    #    options[:foo]   # => true
+    # Define options for this plugin via the <tt>configure</tt> method
+    # in your application manifest:
+    #
+    #    configure(:<%= name %> => {:foo => true})
+    #
+    # Moonshine will autoload plugins, just call the recipe(s) you need in your
+    # manifests:
+    #
+    #    recipe :<%= name %>
+    def <%= name %>(options = {})
+      # define the recipe
+      # options specified with the configure method will be 
+      # automatically available here in the options hash.
+      #    options[:foo]   # => true
+    end
+    
   end
-  
 end
