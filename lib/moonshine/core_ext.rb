8c1fdce9bd3459ded609d9f6f095aac435cdc374
Josh Nichols <josh@railsmachine.com>
2011-05-01 22:42:04 -0400

    Use rocco-style docs for Moonshine::CoreExt.

--- lib/moonshine/core_ext.rb
+++ lib/moonshine/core_ext.rb
@@ -1,12 +1,25 @@
-# These extensions to Pathname let us use Pathname with puppet more easily
-# ie exec "blah", :cwd => Pathname.new('/etc')
-Pathname.class_eval do
-  def =~(pattern)
-    to_s =~ pattern
-  end
+# These extensions to Pathname let us use Pathname with puppet more easily, ie
+#
+#     exec "blah", :cwd => Pathname.new('/etc')
+module Moonshine
+  # These are the extensions we use to deal with reality.
+  module CoreExt
+    ## Pathname
+    #
+    # Pathname is basically awesome. Unfortunately, puppet doesn't deal particularly well with it all the time.
+    # This extension let's it behave more string-like for puppet's sake
+    module Pathname
+      def =~(pattern)
+        to_s =~ pattern
+      end
 
-  def gsub(*args)
-    to_s.gsub(*args)
+      def gsub(*args)
+        to_s.gsub(*args)
+      end
+    end
   end
-  
+end
+
+Pathname.class_eval do
+  include Moonshine::CoreExt::Pathname
 end
