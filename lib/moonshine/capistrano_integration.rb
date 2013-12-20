dc93044f58e2d1787dec3014886824c741b613e6
Bryan Traywick <bryan@railsmachine.com>
2013-12-20 10:26:23 -0500

    Update shadow_puppet version to 0.7.0.

--- lib/moonshine/capistrano_integration.rb
+++ lib/moonshine/capistrano_integration.rb
@@ -689,7 +689,7 @@ module Moonshine
             sudo 'gem install rake --no-rdoc --no-ri' unless fetch(:ruby).start_with?('src200')
             sudo 'gem install i18n --no-rdoc --no-ri' # workaround for missing activesupport-3.0.2 dep on i18n
 
-            shadow_puppet_version = fetch(:shadow_puppet_version, '~> 0.6.6')
+            shadow_puppet_version = fetch(:shadow_puppet_version, '~> 0.7.0')
             sudo "gem install shadow_puppet --no-rdoc --no-ri --version '#{shadow_puppet_version}'"
             if rails_root.join('Gemfile').exist?
               bundler_version = fetch(:bundler_version, '1.1.3')
