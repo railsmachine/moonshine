6fc159fc94bf4c03966568da8f3bcb1ca42b90c1
Bryan Traywick <bryantraywick@gmail.com>
2016-03-30 17:07:14 -0400

    Remove libyaml source compile since all of the rubies now ship with updated versions.

--- lib/moonshine/capistrano_integration.rb
+++ lib/moonshine/capistrano_integration.rb
@@ -509,29 +509,6 @@ module Moonshine
           set :nopuppetrollback, true
         end
 
-        namespace :libyaml do
-          desc "Install libyaml from source."
-          task :install do
-            remove_libyaml_from_apt
-            pv = "0.1.5"
-            p = "yaml-#{pv}"
-            run [
-              'cd /tmp',
-              "sudo rm -rf #{p}* || true",
-              "wget -q http://assets.railsmachine.com/libs/#{p}.tar.gz",
-              "tar xzf #{p}.tar.gz",
-              "cd /tmp/#{p}",
-              './configure --prefix=/usr',
-              'make',
-              'sudo make install'
-            ].join(' && ')
-          end
-
-          task :remove_libyaml_from_apt do
-            sudo 'apt-get remove -q -y ^.*libyaml.* || true'
-          end
-        end
-
         namespace :ruby do
 
           desc <<-DESC
@@ -637,7 +614,6 @@ module Moonshine
 
           task :src193 do
             remove_ruby_from_apt
-            libyaml.install
             pv = "1.9.3-p551"
             p = "ruby-#{pv}"
             run [
@@ -665,7 +641,6 @@ module Moonshine
               upload ruby_patches_path.to_s, "/tmp/moonshine/", :via => :scp, :recursive => true
             end
             remove_ruby_from_apt
-            libyaml.install
             pv = "1.9.3-p551"
             p = "ruby-#{pv}"
             run [
@@ -706,7 +681,6 @@ module Moonshine
 
           task :src200 do
             remove_ruby_from_apt
-            libyaml.install
             pv = "2.0.0-p648"
             p = "ruby-#{pv}"
             run [
@@ -733,7 +707,6 @@ module Moonshine
               upload ruby_patches_path.to_s, "/tmp/moonshine/", :via => :scp, :recursive => true
             end
             remove_ruby_from_apt
-            libyaml.install
             pv = "2.0.0-p648"
             p = "ruby-#{pv}"
             run [
@@ -760,7 +733,6 @@ module Moonshine
 
           task :src21 do
             remove_ruby_from_apt
-            libyaml.install
             pv = "2.1.9"
             p = "ruby-#{pv}"
             run [
@@ -788,7 +760,6 @@ module Moonshine
               upload ruby_patches_path.to_s, "/tmp/moonshine/", :via => :scp, :recursive => true
             end
             remove_ruby_from_apt
-            libyaml.install
             sudo 'apt-get install autoconf -y || true',
             pv = "2.1.8"
             p = "ruby-#{pv}"
@@ -821,7 +792,6 @@ module Moonshine
 
           task :src22 do
             remove_ruby_from_apt
-            libyaml.install
             pv = "2.2.4"
             p = "ruby-#{pv}"
             run [
@@ -845,7 +815,6 @@ module Moonshine
 
           task :src23 do
             remove_ruby_from_apt
-            libyaml.install
             pv = "2.3.0"
             p = "ruby-#{pv}"
             run [
